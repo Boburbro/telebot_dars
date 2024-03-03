2842e1fd3dc81ec0ffd9c96aa7e67edf52ea0d1a
Boburbro <boburotaboyev0@gmail.com>
2024-03-03 14:36:08 +0500

    second

--- ikkinchiBot.py
+++ ikkinchiBot.py
@@ -7,17 +7,14 @@ bot = TeleBot("6736723597:AAElQF73z3lMjIMXmV1dGGj_09tX3QkU1kk")
 
 @bot.message_handler(commands=['start'])
 def start(message: Message):
-    bot.send_message(message.chat.id, "Assalomu alaykum!")
-
+    bot.send_message(chat_id=message.chat.id, text="Assalomu alaykum")
 
 @bot.message_handler()
 def run(message: Message):
+    msg = bot.send_message(chat_id=message.chat.id, text="Tarjima qilinmoqda...")
     text = message.text
+    tr_text = local_translator(text)
+    bot.reply_to(message, tr_text)
+    bot.delete_message(message.chat.id, msg.message_id)
 
-    t = local_translator(text)
-
-    bot.reply_to(message, t) 
-
-
-if __name__ == "__main__":
-    bot.infinity_polling()
+bot.infinity_polling()
